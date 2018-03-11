2bb1546bddf7171f27c4c6505f7b91a3aed444b9
sixgod <sixgod666@163.com>
2018-03-11 22:29:34 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import mHeader from 'components/m-header/m-header'
+export default {
+  name: 'App',
+  components: {
+    mHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="m-header">
+    <div class="logo"></div>
+    <h1 class="text">Koala Music</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'm-header'
+}
+</script>
+
+<style lang="stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+.m-header {
+  position relative
+  height: 44px
+  font-size: 0
+  text-align: center
+  color: $color-theme
+  .logo {
+    display: inline-block
+    width: 30px
+    height 32px
+    background-size 30px 32px
+    vertical-align top
+    margin-top 6px
+    margin-right 9px
+    bg-image('logo')
+  }
+  .text {
+    display inline-block
+    font-size $font-size-large
+    vertical-align top
+    line-height 44px
+  }
+}
+</style>
